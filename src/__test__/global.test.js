b4c9e4063af2110440d1ad771a29b33e3b37c19c
LizzColDev <lquintero28@live.com>
2023-03-17 15:00:44 -0500

    Added array/boolean unit test

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -1,5 +1,33 @@
 const text = 'Hola Mundo';
+const fruits = ['manzana', 'pera', 'banano']
 
+// string
 test('debe contener un texto', ()=>{
     expect(text).toMatch(/Mundo/);
 })
+
+// array
+test('Tenemos un banano?', ()=>{
+    expect(fruits).toContain('banano');
+});
+
+// numbers
+test('Mayor que', ()=>{
+    expect(10).toBeGreaterThan(9);
+});
+
+//Boolean
+test('Verdadero', () => {
+    expect(true).toBeTruthy
+})
+
+//callback
+const reverseString = (str, callback) => {
+    callback(str.split('').reverse().join(''))
+};
+
+test('Probar un CallBack', () => {
+    reverseString('Hola', (str) =>{
+        expect(str).toBe('aloH')
+    });
+});
